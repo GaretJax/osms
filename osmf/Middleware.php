7c6b35d4e0e7104c140e7a1e98fd01a1ad1e427b
Jonathan Stoppani <jonathan@stoppani.name>
2012-04-03 12:36:48 +0200

    Made middleware class abstract

--- osmf/Middleware.php
+++ osmf/Middleware.php
@@ -1,7 +1,13 @@
 <?php namespace osmf;
 
+/**
+ * Middlewares im oSMF are greatly inspired by Django's one.
+ * Refer to the original documentation for more information:
+ * https://docs.djangoproject.com/en/dev/topics/http/middleware/
+ */
 
-class Middleware
+
+abstract class Middleware
 {
 	public function process_request($request)
 	{
